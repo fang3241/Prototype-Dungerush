cae6b18c81c3ce54b799ce9ba8bf186169da98b6
fang3241 <mikhsanmf@gmail.com>
2023-07-21 16:14:11 +0700

    Init Movement
    baru nambahin movement, masih ada bug jalan mundur malah maju

---
+++ SimpleMovement/Assets/ThirdPersonMovement.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public Transform cam;
+    public float speed;
+
+    public float turnSmoothTime;
+
+    private float turnSmoothVelocity;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 dir = new Vector3(horizontal, 0f, vertical).normalized;
+        Debug.Log(dir);
+        if(dir.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(dir.x, dir.y) * Mathf.Rad2Deg + cam.eulerAngles.y;
+
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+
+
+            Vector3 movedir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+            controller.Move(dir * speed * Time.deltaTime);
+        }
+
+    }
+
+}
